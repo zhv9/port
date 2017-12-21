b3eb72508e186c3503b6c1a96e5150e9bc3abc47
zhangyu <zhangyu@micromach.cn>
2017-12-21 18:03:49 +0800

    添加定义

--- src/defines.py
+++ src/defines.py
@@ -18,3 +18,7 @@ io15 = 38
 
 io_defines = dict(io0=7, io1=11, io2=12, io3=13, io4=15, io5=16, io6=18, io7=22,
                   io8=29, io9=31, io10=32, io11=33, io12=35, io13=36, io14=37, io15=38)
+
+baudrate_list = [300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 43000, 56000, 57600, 115200]
+
+ACTIVE_DEVICE = 'active_device'
