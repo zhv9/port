396c06b538f38530701b6128c26d17a4807b9d23
zhangyu <zhangyu@micromach.cn>
2018-01-05 16:47:29 +0800

    前端初始代码

---
+++ src/app/frontend/src/views/index.vue
@@ -0,0 +1,128 @@
+<style scoped>
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+    .layout-header-bar{
+        background: #fff;
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    }
+</style>
+<template>
+    <div class="layout">
+        <Sider :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}">
+            <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
+                <Submenu name="1">
+                    <template slot="title">
+                        <Icon size="20" type="arrow-swap"></Icon>
+                        IO控制
+                    </template>
+                    <MenuItem name="1-1" @click.native="menuClick">
+                    <Icon size="20" type="settings"></Icon>
+                    配置
+                    </MenuItem>
+                    <MenuItem name="1-2" @click.native="menuClick">
+                    <Icon size="20" type="nuclear"></Icon>
+                    数据
+                    </MenuItem>
+                    <MenuItem name="1-3" @click.native="menuClick">
+                    <Icon size="20" type="erlenmeyer-flask"></Icon>
+                    其他
+                    </MenuItem>
+                </Submenu>
+                <Submenu name="2">
+                    <template slot="title">
+                        <Icon size="20" type="shuffle"></Icon>
+                        串口控制
+                    </template>
+                    <MenuItem name="2-1" @click.native="menuClick">
+                    <Icon size="20" type="settings"></Icon>
+                    配置
+                    </MenuItem>
+                    <MenuItem name="2-2" @click.native="menuClick">
+                    <Icon size="20" type="nuclear"></Icon>
+                    数据
+                    </MenuItem>
+                    <MenuItem name="2-3" @click.native="menuClick">
+                    <Icon size="20" type="erlenmeyer-flask"></Icon>
+                    其他
+                    </MenuItem>
+                </Submenu>
+                <Submenu name="3">
+                    <template slot="title">
+                        <Icon size="20" type="android-bus"></Icon>
+                        Modbus控制
+                    </template>
+                    <MenuItem name="3-1" @click.native="menuClick">
+                    <Icon size="20" type="settings"></Icon>
+                    配置
+                    </MenuItem>
+                    <MenuItem name="3-2" @click.native="menuClick">
+                    <Icon size="20" type="nuclear"></Icon>
+                    数据
+                    </MenuItem>
+                    <MenuItem name="3-3" @click.native="menuClick">
+                    <Icon size="20" type="erlenmeyer-flask"></Icon>
+                    其他
+                    </MenuItem>
+                </Submenu>
+
+            </Menu>
+        </Sider>
+        <Layout :style="{marginLeft: '200px'}">
+            <Header :style="{background: '#fff', boxShadow: '0 2px 3px 2px rgba(0,0,0,.1)'}"></Header>
+            <Content :style="{padding: '0 16px 16px'}">
+                <Breadcrumb :style="{margin: '16px 0'}">
+                    <BreadcrumbItem>Home</BreadcrumbItem>
+                    <BreadcrumbItem>{{components}}</BreadcrumbItem>
+                    <BreadcrumbItem>{{layout}}</BreadcrumbItem>
+                </Breadcrumb>
+                <Card>
+                    <div style="height: 600px">{{content}}</div>
+                </Card>
+            </Content>
+        </Layout>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                components:'',
+                layout:'',
+                contents:{
+                    'IO控制':{
+                        '配置':'ioSetting',
+                        '数据':'ioData',
+                        '其他':'ioOther'
+                    },
+                    '串口控制':{
+                        '配置':'serialSetting',
+                        '数据':'serialData',
+                        '其他':'serialOther'
+                    },
+                    'ModBus控制':{
+                        '配置':'modbusSetting',
+                        '数据':'modbusData',
+                        '其他':'modbusOther'
+                    }
+                },
+                content:'cccccc'
+            }
+        },
+        methods:{
+            menuClick(event){
+                var components = event.currentTarget.parentElement.parentNode.firstChild.innerText
+                                        .replace(/^(\s|\u00A0)+/,'').replace(/(\s|\u00A0)+$/,'');
+                var layout = event.currentTarget.innerText
+                                        .replace(/^(\s|\u00A0)+/,'').replace(/(\s|\u00A0)+$/,'');
+                this.components = components;
+                this.layout = layout;
+                console.log(this.contents[components][layout]);
+            }
+        }
+    }
+</script>
