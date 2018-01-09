830fc3f6e559f1fff4566a8bd7c905b53bb6c260
zhv <zh.v@live.cn>
2018-01-09 19:21:06 +0800

    添加路由

--- src/app/frontend/src/router.js
+++ src/app/frontend/src/router.js
@@ -2,9 +2,23 @@ const routers = [
     {
         path: '/',
         meta: {
-            title: ''
+            title: 'home'
         },
         component: (resolve) => require(['./views/index.vue'], resolve)
+    },
+    {
+        path: '/serial/setting',
+        meta: {
+            title: 'SerialSetting'
+        },
+        component: (resolve) => require(['./views/SerialSetting.vue'], resolve)
+    },
+    {
+        path: '/serial/data',
+        meta: {
+            title: 'SerialData'
+        },
+        component: (resolve) => require(['./views/SerialData.vue'], resolve)
     }
 ];
 export default routers;
